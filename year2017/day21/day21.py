172f19885b5324a60b69b996354e75745b6ff4e0
Eric McKinley <eric.mckinley@gamesys.co.uk>
2018-01-04 18:37:04 +0000

    WIP

--- year2017/day21/day21.py
+++ year2017/day21/day21.py
@@ -1,6 +1,24 @@
-def runProgram(grid):
+class EnhancementData:
+    def __init__(self, info):
+        info =info.strip("\n")
+        self.input = info[0:info.index("=") -1]
+        self.output = info[info.index(">") + 2:len(info)]
 
-    for i in range(0,5):
+    def __str__(self):
+        return "EnhancementData: in: [{}], out: [{}]".format(self.input, self.output)
+
+    def 
+
+def runProgram(inputfile):
+    with open(inputfile) as f:
+        lines = f.readlines()
+
+    for l in lines:
+        print(EnhancementData(l))
+
+def runProgram1(grid):
+
+    for i in range(0,7):
         print(grid)
         grid = enhance_grid(grid)
         print("")
@@ -12,7 +30,7 @@ def enhance_grid(lines):
     gridsize = len(grid)
     if gridsize % 3 == 0:
         nextgrid = list()
-        for z in range(0, (gridsize / 3) * 4):
+        for z in range(0, int(gridsize / 3) * 4):
             nextgrid.append("")
 
         for y in range(0, gridsize, 3):
@@ -20,12 +38,12 @@ def enhance_grid(lines):
                 group = grid[y][x:x + 3] + "\n" + grid[y + 1][x:x + 3] + "\n" + grid[y + 2][x:x + 3]
                 fourxfour = enhance_3x3(group).split("\n")
                 for fi in range(0, len(fourxfour)) :
-                    nextgrid[((y / 3) * 4) + fi] += (fourxfour[fi])
+                    nextgrid[(int(y / 3) * 4) + fi] += (fourxfour[fi])
         return "\n".join(nextgrid)
 
     else:
         nextgrid = list()
-        for z in range(0, (gridsize / 2) * 3):
+        for z in range(0, int(gridsize / 2) * 3):
             nextgrid.append("")
 
         for y in range(0, gridsize, 2):
@@ -33,7 +51,7 @@ def enhance_grid(lines):
                 group = grid[y][x:x + 2] + "\n" + grid[y + 1][x:x + 2]
                 threexthree = enhance_2x2(group).split("\n")
                 for ti in range(0, len(threexthree)) :
-                    nextgrid[((y / 2) * 3) + ti] += (threexthree[ti])
+                    nextgrid[(int(y / 2) * 3) + ti] += (threexthree[ti])
         return "\n".join(nextgrid)
 
 def enhance_2x2(twoxtwo):
@@ -44,6 +62,7 @@ def enhance_3x3(threexthree):
     return "hhhh\niiii\njjjj\nkkkk"
 
 
-runProgram("abcd\nefgh\nijkl\nmnop")
+runProgram("input.txt")
+# runProgram("abcd\nefgh\nijkl\nmnop")
 # print("ODS")
 # runProgram("abcdef\nefghij\nijklmn\nmnopqr\nstuvwx\nyzwx23")
