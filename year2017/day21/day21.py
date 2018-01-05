8e3ad644a4067c8d9fdd59c1ede020c977093da9
Eric McKinley <mckinley.eric@yahoo.co.uk>
2018-01-05 08:51:06 +0000

    Day21P1

--- year2017/day21/day21.py
+++ year2017/day21/day21.py
@@ -1,31 +1,28 @@
 class EnhancementData:
     def __init__(self, info):
-        info =info.strip("\n")
-        self.input = info[0:info.index("=") -1]
+        info = info.strip("\n")
+        self.input = info[0:info.index("=") - 1]
         self.output = info[info.index(">") + 2:len(info)]
+
+    def ruleMatches(self, grid):
+        fgrid = grid
+        for i in range(0, 5):
+            if grid == self.input:
+                return True
+            grid = rotate_block(grid)
+        grid = mirror_block(fgrid)
+        for i in range(0, 5):
+            if grid == self.input:
+                return True
+            grid = rotate_block(grid)
+        return False
 
     def __str__(self):
         return "EnhancementData: in: [{}], out: [{}]".format(self.input, self.output)
 
-    def 
 
-def runProgram(inputfile):
-    with open(inputfile) as f:
-        lines = f.readlines()
-
-    for l in lines:
-        print(EnhancementData(l))
-
-def runProgram1(grid):
-
-    for i in range(0,7):
-        print(grid)
-        grid = enhance_grid(grid)
-        print("")
-    print(grid)
-    
-def enhance_grid(lines):
-    grid = lines.split("\n")
+def enhance_grid(lines, enhancements):
+    grid = lines.split("/")
 
     gridsize = len(grid)
     if gridsize % 3 == 0:
@@ -35,11 +32,11 @@ def enhance_grid(lines):
 
         for y in range(0, gridsize, 3):
             for x in range(0, gridsize, 3):
-                group = grid[y][x:x + 3] + "\n" + grid[y + 1][x:x + 3] + "\n" + grid[y + 2][x:x + 3]
-                fourxfour = enhance_3x3(group).split("\n")
-                for fi in range(0, len(fourxfour)) :
+                group = grid[y][x:x + 3] + "/" + grid[y + 1][x:x + 3] + "/" + grid[y + 2][x:x + 3]
+                fourxfour = enhance_pattern(group, enhancements).split("/")
+                for fi in range(0, len(fourxfour)):
                     nextgrid[(int(y / 3) * 4) + fi] += (fourxfour[fi])
-        return "\n".join(nextgrid)
+        return "/".join(nextgrid)
 
     else:
         nextgrid = list()
@@ -48,21 +45,92 @@ def enhance_grid(lines):
 
         for y in range(0, gridsize, 2):
             for x in range(0, gridsize, 2):
-                group = grid[y][x:x + 2] + "\n" + grid[y + 1][x:x + 2]
-                threexthree = enhance_2x2(group).split("\n")
-                for ti in range(0, len(threexthree)) :
+                group = grid[y][x:x + 2] + "/" + grid[y + 1][x:x + 2]
+                threexthree = enhance_pattern(group, enhancements).split("/")
+                for ti in range(0, len(threexthree)):
                     nextgrid[(int(y / 2) * 3) + ti] += (threexthree[ti])
-        return "\n".join(nextgrid)
-
-def enhance_2x2(twoxtwo):
-    return "aaa\nbbb\nccc"
+        return "/".join(nextgrid)
 
 
-def enhance_3x3(threexthree):
-    return "hhhh\niiii\njjjj\nkkkk"
+def enhance_pattern(pattern, enhancements):
+    for enhancement in enhancements:
+        if enhancement.ruleMatches(pattern): return enhancement.output
+    print("NOT FOUND: {}".format(pattern))
 
 
-runProgram("input.txt")
-# runProgram("abcd\nefgh\nijkl\nmnop")
-# print("ODS")
-# runProgram("abcdef\nefghij\nijklmn\nmnopqr\nstuvwx\nyzwx23")
+# abc
+# def
+# ghi
+#
+# gda
+# heb
+# ifc
+
+def rotate_block(block):
+    lines = block.split("/")
+    if len(lines) == 3:
+        return rotate_3x3_block(lines)
+    else:
+        return rotate_2x2_block(lines)
+
+def mirror_block(block):
+    lines = block.split("/")
+    if len(lines) == 3:
+        return mirror_3x3_block(lines)
+    else:
+        return mirror_2x2_block(lines)
+
+
+
+def rotate_2x2_block(lines):
+    rotated = list()
+    rotated.append(lines[1][0:1] + lines[0][0:1])
+    rotated.append(lines[1][1:2] + lines[0][1:2])
+    return "/".join(rotated)
+
+
+def rotate_3x3_block(lines):
+    rotated = list()
+    rotated.append(lines[2][0:1] + lines[1][0:1] + lines[0][0:1])
+    rotated.append(lines[2][1:2] + lines[1][1:2] + lines[0][1:2])
+    rotated.append(lines[2][2:3] + lines[1][2:3] + lines[0][2:3])
+    return "/".join(rotated)
+
+
+def mirror_2x2_block(lines):
+    rotated = list()
+    rotated.append(lines[0][1:2] + lines[0][0:1])
+    rotated.append(lines[1][1:2] + lines[1][0:1])
+    return "/".join(rotated)
+
+
+def mirror_3x3_block(lines):
+    rotated = list()
+    rotated.append(lines[0][2:3] + lines[0][1:2] + lines[0][0:1])
+    rotated.append(lines[1][2:3] + lines[1][1:2] + lines[1][0:1])
+    rotated.append(lines[2][2:3] + lines[2][1:2] + lines[2][0:1])
+    return "/".join(rotated)
+
+
+def runProgram(inputfile, startgrid, rotations):
+    with open(inputfile) as f:
+        lines = f.readlines()
+
+    enhancements = list()
+    for l in lines:
+        enhancements.append(EnhancementData(l))
+
+    for x in range(0, rotations):
+        startgrid = enhance_grid(startgrid, enhancements)
+
+    endgrid = startgrid.replace("/", "\n")
+
+    print("{} rules after {} rotations, gives pixels {}:".format(inputfile, rotations, endgrid.count("#")))
+    print(endgrid)
+
+firstgrid = ".#./..#/###"
+print("Part 1 sample")
+runProgram("sample.txt", firstgrid, 2)
+print("")
+print("Part 1")
+runProgram("input.txt", firstgrid, 5)
