d9ae725fb0567e36a84339b2522420a258df7476
Eric McKinley <eric.mckinley@gamesys.co.uk>
2018-01-05 17:33:38 +0000

    Day21 Part1 and 2 improved speed

--- year2017/day21/day21.py
+++ year2017/day21/day21.py
@@ -1,31 +1,46 @@
-class EnhancementData:
-    def __init__(self, info):
-        info = info.strip("\n")
-        self.input = info[0:info.index("=") - 1]
-        self.output = info[info.index(">") + 2:len(info)]
+import datetime
 
-    def ruleMatches(self, grid):
-        fgrid = grid
-        for i in range(0, 5):
-            if grid == self.input:
-                return True
-            grid = rotate_block(grid)
-        grid = mirror_block(fgrid)
-        for i in range(0, 5):
-            if grid == self.input:
-                return True
-            grid = rotate_block(grid)
-        return False
+class EnhancementData:
+    def __init__(self, in_pattern, out_pattern):
+        self.input = in_pattern
+        self.output = out_pattern
 
     def __str__(self):
         return "EnhancementData: in: [{}], out: [{}]".format(self.input, self.output)
+
+def cube_rotations(grid):
+    rotations = list()
+    rotations.append(grid)
+    fgrid = grid
+    for x in range(0, 4):
+        grid = rotate_block(grid)
+        rotations.append(grid)
+
+    grid = mirror_block(fgrid)
+    rotations.append(grid)
+    for x in range(0, 4):
+        grid = rotate_block(grid)
+        rotations.append(grid)
+    return rotations
 
 
 def enhance_grid(lines, enhancements):
     grid = lines.split("/")
 
     gridsize = len(grid)
-    if gridsize % 3 == 0:
+    if gridsize % 2 == 0:
+        nextgrid = list()
+        for z in range(0, int(gridsize / 2) * 3):
+            nextgrid.append("")
+
+        for y in range(0, gridsize, 2):
+            for x in range(0, gridsize, 2):
+                group = grid[y][x:x + 2] + "/" + grid[y + 1][x:x + 2]
+                threexthree = enhance_pattern(group, enhancements).split("/")
+                for ti in range(0, len(threexthree)):
+                    nextgrid[(int(y / 2) * 3) + ti] += (threexthree[ti])
+        return "/".join(nextgrid)
+    else:
         nextgrid = list()
         for z in range(0, int(gridsize / 3) * 4):
             nextgrid.append("")
@@ -38,33 +53,10 @@ def enhance_grid(lines, enhancements):
                     nextgrid[(int(y / 3) * 4) + fi] += (fourxfour[fi])
         return "/".join(nextgrid)
 
-    else:
-        nextgrid = list()
-        for z in range(0, int(gridsize / 2) * 3):
-            nextgrid.append("")
-
-        for y in range(0, gridsize, 2):
-            for x in range(0, gridsize, 2):
-                group = grid[y][x:x + 2] + "/" + grid[y + 1][x:x + 2]
-                threexthree = enhance_pattern(group, enhancements).split("/")
-                for ti in range(0, len(threexthree)):
-                    nextgrid[(int(y / 2) * 3) + ti] += (threexthree[ti])
-        return "/".join(nextgrid)
-
-
 def enhance_pattern(pattern, enhancements):
     for enhancement in enhancements:
-        if enhancement.ruleMatches(pattern): return enhancement.output
-    print("NOT FOUND: {}".format(pattern))
-
-
-# abc
-# def
-# ghi
-#
-# gda
-# heb
-# ifc
+        if pattern == enhancement.input: return enhancement.output
+    print("PATTERN NOT FOUND: {}".format(pattern))
 
 def rotate_block(block):
     lines = block.split("/")
@@ -118,19 +110,29 @@ def runProgram(inputfile, startgrid, rotations):
 
     enhancements = list()
     for l in lines:
-        enhancements.append(EnhancementData(l))
 
+        info = l.strip("\n")
+        in_pattern = info[0:info.index("=") - 1]
+        out_pattern = info[info.index(">") + 2:len(info)]
+
+        enhdata = EnhancementData(in_pattern, out_pattern)
+        enhancements.append(enhdata)
+        rotatations = cube_rotations(in_pattern)
+        for rotatation in rotatations:
+            enhancements.append(EnhancementData(rotatation, out_pattern))
+
+    starttime = datetime.datetime.now()
     for x in range(0, rotations):
         startgrid = enhance_grid(startgrid, enhancements)
-
+    endtime = datetime.datetime.now()
     endgrid = startgrid.replace("/", "\n")
 
-    print("{} rules after {} rotations, gives pixels {}:".format(inputfile, rotations, endgrid.count("#")))
-    print(endgrid)
+    print("{} rules after {} rotations, ran in {}, gives pixels {}:".format(inputfile, rotations,endtime - starttime, endgrid.count("#")))
 
 firstgrid = ".#./..#/###"
 print("Part 1 sample")
 runProgram("sample.txt", firstgrid, 2)
-print("")
-print("Part 1")
+print("\nPart 1")
 runProgram("input.txt", firstgrid, 5)
+print("\nPart 2")
+runProgram("input.txt", firstgrid, 18)
